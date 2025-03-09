322c7675441790e6428822f2b8e830e766b25897
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-03-09 16:03:04 +0100

    cors - update

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const connectDB = require('./db');
-const authRoutes = require('./App/routes/authRoutes');
-const userRoutes = require('./App/routes/userRoutes');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db");
+const authRoutes = require("./App/routes/authRoutes");
+const userRoutes = require("./App/routes/userRoutes");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 connectDB();
+
+// CORS configuration
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://ai-stylist.netlify.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // all api routes
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 // start server
 app.listen(5000, () => console.log("Server running on port 5000"));
